0c06d57d5d7bae8161b994a8f82c7dacd83ac387
Voloshynb <voloshynbohdann@gmail.com>
2024-06-17 18:46:20 -0700

    Fix: New changes added

--- load_balancer.tf
+++ load_balancer.tf
@@ -33,14 +33,14 @@ resource "aws_lb_listener" "web_lb_listener" {
   }
 }
 
-resource "aws_lb_target_group_attachment" "web1" {
+resource "aws_lb_target_group_attachment" "production1" {
   target_group_arn = aws_lb_target_group.web_tg.arn
-  target_id        = aws_instance.web1.id
+  target_id        = aws_instance.production1.id
   port             = 80
 }
 
-resource "aws_lb_target_group_attachment" "web2" {
+resource "aws_lb_target_group_attachment" "production2" {
   target_group_arn = aws_lb_target_group.web_tg.arn
-  target_id        = aws_instance.web2.id
+  target_id        = aws_instance.production2.id
   port             = 80
 }
--- security_group.tf
+++ security_group.tf
@@ -24,4 +24,11 @@ resource "aws_security_group" "web_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
